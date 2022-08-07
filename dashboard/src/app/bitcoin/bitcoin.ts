5d62d0708458ec60c5ebdd2007197cfe0112baee
Javi <nhitbh@gmail.com>
2022-08-07 23:45:00 +0200

    Show bitcoin information every 2 seconds

--- dashboard/src/app/bitcoin/bitcoin.ts
+++ dashboard/src/app/bitcoin/bitcoin.ts
@@ -2,7 +2,8 @@ export class Bitcoin {
   constructor(
     public BlockchainInfo: BlockchainInfo,
     public NetworkInfo: NetworkInfo,
-    public NetTotals: NetTotals
+    public NetTotals: NetTotals,
+    public ChainTxStats: ChainTxStats
   ) {
   }
 }
@@ -13,7 +14,10 @@ export class BlockchainInfo {
     public blocks: number,
     public headers: number,
     public bestblockhash: string,
-    public size_on_disk: number
+    public size_on_disk: number,
+    public chain: string,
+    public chainwork: string,
+    public difficulty: number
   ) {
   }
 }
@@ -32,6 +36,14 @@ export class NetTotals {
   constructor(
     public totalbytesrecv: number,
     public totalbytessent: number
+  ) {
+  }
+}
+
+export class ChainTxStats {
+  constructor(
+    public txcount: number,
+    public txrate: number
   ) {
   }
 }
--- dashboard/src/app/bitcoin/info/info.component.ts
+++ dashboard/src/app/bitcoin/info/info.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Bitcoin} from "../bitcoin";
 import {BitcoinService} from "../bitcoin.service";
 
@@ -7,18 +7,35 @@ import {BitcoinService} from "../bitcoin.service";
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent implements OnInit, OnDestroy {
   model: Bitcoin | undefined
+  interval: number | undefined
 
   constructor(private btcService: BitcoinService) {
+    this.interval = setInterval(() => {
+      this.fetchBitcoinInfo()
+    }, 2000)
   }
 
   ngOnInit(): void {
-    this.btcService.fetchInfo()
-      .subscribe(data => {
-        this.model = data
-      }, error => {
-        console.error(error)
-      })
+    this.fetchBitcoinInfo()
+  }
+
+  private fetchBitcoinInfo(): void {
+    this.btcService.fetchInfo().subscribe(data => {
+      this.updateBitcoinInfo(data)
+    }, error => {
+      console.error(error)
+    })
+  }
+
+  private updateBitcoinInfo(info: Bitcoin): void {
+    this.model = info
+  }
+
+  ngOnDestroy() {
+    if (this.interval === undefined)
+      return
+    clearInterval(this.interval)
   }
 }
